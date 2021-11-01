951b8f8a8263a60d1441ecd7a43d53a7737c0e10
EinoHR <EinoHR@noreply.codeberg.org>
2021-11-01 17:51:07 +0200

    Test different players, split video player init

--- lib/widgets/lbry_video_player.dart
+++ lib/widgets/lbry_video_player.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:better_player/better_player.dart';
+// import 'package:chewie/chewie.dart';
+// import 'package:video_player/video_player.dart';
 
 class LbryVideoPlayer extends StatefulWidget {
   const LbryVideoPlayer({Key? key, required this.streamingUrl}) : super(key: key);
@@ -12,10 +14,30 @@ class LbryVideoPlayer extends StatefulWidget {
 class _LbryVideoPlayerState extends State<LbryVideoPlayer> {
   late BetterPlayerController _betterPlayerController;
   late BetterPlayerDataSource _betterPlayerDataSource;
+  
+  // late final VideoPlayerController _videoPlayerController;
+  // late final ChewieController _chewieController;
 
   @override
   void initState() {
     super.initState();
+    initVideoPlayer();
+  }
+
+  void initVideoPlayer() {
+
+    // _videoPlayerController = VideoPlayerController.network(widget.streamingUrl, formatHint: VideoFormat.hls,);
+    // // _videoPlayerController.initialize();
+
+    // _chewieController = ChewieController(
+    //   videoPlayerController: _videoPlayerController,
+    //   autoPlay: true,
+    //   looping: true,
+    //   aspectRatio: 16 /9,
+    //   allowFullScreen: true,
+    // );
+
+
     BetterPlayerConfiguration betterPlayerConfiguration =
     BetterPlayerConfiguration(
       aspectRatio: 16 / 9,
@@ -30,13 +52,21 @@ class _LbryVideoPlayerState extends State<LbryVideoPlayer> {
     _betterPlayerController = BetterPlayerController(betterPlayerConfiguration);
     _betterPlayerController.setupDataSource(_betterPlayerDataSource);
   }
+
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 16 / 9,
-      child: BetterPlayer(controller: _betterPlayerController),
+      child: BetterPlayer(controller: _betterPlayerController,)
     );
   }
+
+  // @override
+  // void dispose() {
+  //   _videoPlayerController.dispose();
+  //   _chewieController.dispose();
+  //   super.dispose();
+  // }
 }
 
 
