0069fdc6311182c572f288e04449f7e269752f7e
EinoHR <EinoHR@noreply.codeberg.org>
2021-11-01 17:51:52 +0200

    Add theming.

---
+++ lib/theme.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+Map colors = {
+  "accent" :_colorFromHex("#E9436F"),
+  "background1": _colorFromHex("#1A1C23"),
+  "background2": _colorFromHex("#232530"),
+  "textColor": _colorFromHex("#FFFFFF")
+  };
+
+Color _colorFromHex(String hexColor) {
+  hexColor = hexColor.replaceAll("#", "");
+  if (hexColor.length == 6) {
+    hexColor = "FF" + hexColor;
+  }
+  if (hexColor.length == 8) {
+    return Color(int.parse("0x$hexColor"));
+  } else {
+    throw("Bad hex color length");
+  }
+}
